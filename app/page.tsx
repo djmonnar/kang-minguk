7c01d4c1ab8bf78aa44e2ee0f2a4fabd4ff4ba81
djmonnar <djmonnar4@gmail.com>
2026-05-05 02:27:49 +0900

    Connect profile and constituency sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { CitizenParticipation } from "@/components/CitizenParticipation";
+import { ConstituencySection } from "@/components/ConstituencySection";
 import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
 import { JinjuMapSection } from "@/components/JinjuMapSection";
 import { LatestActivities } from "@/components/LatestActivities";
 import { MediaGallery } from "@/components/MediaGallery";
 import { PolicyCards } from "@/components/PolicyCards";
+import { ProfileInfoSection } from "@/components/ProfileInfoSection";
 import { QuickLinks } from "@/components/QuickLinks";
 
 export default function Home() {
@@ -12,6 +14,8 @@ export default function Home() {
     <main>
       <HeroSection />
       <QuickLinks />
+      <ProfileInfoSection />
+      <ConstituencySection />
       <JinjuMapSection />
       <PolicyCards />
       <LatestActivities />
